62c93b24c32b1fc770faae1e1d25d1ac97b9f36f
Quế Nguyệt <quentn0620@gmail.com>
2024-01-16 00:05:39 +0700

    Frontend: Create ThemeContext

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useContext } from "react";
+import { ThemeContext } from "./context/theme.context";
 
 const App = () => {
-  return <div className="app">App</div>;
+  const { darkMode } = useContext(ThemeContext);
+
+  const appStyles = darkMode ? "app dark" : "app";
+
+  return <div className={appStyles}>App</div>;
 };
 
 export default App;
