2b0589753ce5f100fcece9a31ae444c251e9c507
lisana0620 <nguyenloan06201011@gmail.com>
2024-01-17 21:35:53 +0700

    Backend: Add Controllers and API for Models: AcademicYears, ActionTypes, Activities, ActivityCategories, ActivityParticipations, ActivitySessions, AttendanceComplaints

---
+++ backend/Controllers/AcademicYearsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AcademicYearsController : ControllerBase
+    {
+        private readonly SocialWorkDbContext _context;
+
+        public AcademicYearsController(SocialWorkDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AcademicYears
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AcademicYear>>> GetAcademicYears()
+        {
+            return await _context.AcademicYears.ToListAsync();
+        }
+
+        // GET: api/AcademicYears/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AcademicYear>> GetAcademicYear(int id)
+        {
+            var academicYear = await _context.AcademicYears.FindAsync(id);
+
+            if (academicYear == null)
+            {
+                return NotFound();
+            }
+
+            return academicYear;
+        }
+
+        // PUT: api/AcademicYears/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAcademicYear(int id, AcademicYear academicYear)
+        {
+            if (id != academicYear.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(academicYear).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AcademicYearExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AcademicYears
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AcademicYear>> PostAcademicYear(AcademicYear academicYear)
+        {
+            _context.AcademicYears.Add(academicYear);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAcademicYear", new { id = academicYear.Id }, academicYear);
+        }
+
+        // DELETE: api/AcademicYears/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAcademicYear(int id)
+        {
+            var academicYear = await _context.AcademicYears.FindAsync(id);
+            if (academicYear == null)
+            {
+                return NotFound();
+            }
+
+            _context.AcademicYears.Remove(academicYear);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AcademicYearExists(int id)
+        {
+            return _context.AcademicYears.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ backend/Controllers/ActivityParticipationsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityParticipationsController : ControllerBase
+    {
+        private readonly SocialWorkDbContext _context;
+
+        public ActivityParticipationsController(SocialWorkDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ActivityParticipations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ActivityParticipation>>> GetActivityParticipations()
+        {
+            return await _context.ActivityParticipations.ToListAsync();
+        }
+
+        // GET: api/ActivityParticipations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ActivityParticipation>> GetActivityParticipation(int id)
+        {
+            var activityParticipation = await _context.ActivityParticipations.FindAsync(id);
+
+            if (activityParticipation == null)
+            {
+                return NotFound();
+            }
+
+            return activityParticipation;
+        }
+
+        // PUT: api/ActivityParticipations/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutActivityParticipation(int id, ActivityParticipation activityParticipation)
+        {
+            if (id != activityParticipation.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(activityParticipation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ActivityParticipationExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ActivityParticipations
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<ActivityParticipation>> PostActivityParticipation(ActivityParticipation activityParticipation)
+        {
+            _context.ActivityParticipations.Add(activityParticipation);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetActivityParticipation", new { id = activityParticipation.Id }, activityParticipation);
+        }
+
+        // DELETE: api/ActivityParticipations/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivityParticipation(int id)
+        {
+            var activityParticipation = await _context.ActivityParticipations.FindAsync(id);
+            if (activityParticipation == null)
+            {
+                return NotFound();
+            }
+
+            _context.ActivityParticipations.Remove(activityParticipation);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ActivityParticipationExists(int id)
+        {
+            return _context.ActivityParticipations.Any(e => e.Id == id);
+        }
+    }
+}
