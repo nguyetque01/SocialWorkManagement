1fa6e8ce9b20d759ce696228db4cf53f36a6e82e
TranThiThuyVi <120365745+TranThiThuyVi@users.noreply.github.com>
2024-01-17 21:55:49 +0700

    Backend: Add Controllers and API for Models: Notifications, NotificationTypes, Permissions, RecordHistories, Roles, RolePermissions, Users
    * Backend: Add Controllers and API for Models: Notifications, NotificationTypes, Permissions, RecordHistories, Roles, RolePermissions, Users
    
    * Update appsettings.json
    (return original)
    
    ---------
    
    Co-authored-by: Nguyet Que <91485135+nguyetque01@users.noreply.github.com>

---
+++ backend/Controllers/NotificationTypesController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationTypesController : ControllerBase
+    {
+        private readonly SocialWorkDbContext _context;
+
+        public NotificationTypesController(SocialWorkDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/NotificationTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<NotificationType>>> GetNotificationTypes()
+        {
+            return await _context.NotificationTypes.ToListAsync();
+        }
+
+        // GET: api/NotificationTypes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NotificationType>> GetNotificationType(int id)
+        {
+            var notificationType = await _context.NotificationTypes.FindAsync(id);
+
+            if (notificationType == null)
+            {
+                return NotFound();
+            }
+
+            return notificationType;
+        }
+
+        // PUT: api/NotificationTypes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutNotificationType(int id, NotificationType notificationType)
+        {
+            if (id != notificationType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(notificationType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!NotificationTypeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/NotificationTypes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<NotificationType>> PostNotificationType(NotificationType notificationType)
+        {
+            _context.NotificationTypes.Add(notificationType);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetNotificationType", new { id = notificationType.Id }, notificationType);
+        }
+
+        // DELETE: api/NotificationTypes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteNotificationType(int id)
+        {
+            var notificationType = await _context.NotificationTypes.FindAsync(id);
+            if (notificationType == null)
+            {
+                return NotFound();
+            }
+
+            _context.NotificationTypes.Remove(notificationType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool NotificationTypeExists(int id)
+        {
+            return _context.NotificationTypes.Any(e => e.Id == id);
+        }
+    }
+}
