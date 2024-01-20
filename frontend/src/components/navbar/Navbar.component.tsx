416837edd9ca8e8f79386e87938f51304fe05c74
Nguyet Que <91485135+nguyetque01@users.noreply.github.com>
2024-01-20 10:54:01 +0700

    Frontend: Create and Style Navbar, Install and Use Package Material Icons (#3)

---
+++ frontend/src/components/navbar/Navbar.component.tsx
@@ -0,0 +1,55 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { Menu, LightMode, DarkMode } from "@mui/icons-material";
+import { ToggleButton } from "@mui/material";
+import { ThemeContext } from "../../context/theme.context";
+
+const links = [
+  { href: "", label: "Home" },
+  { href: "/activities", label: "Activities" },
+  { href: "/activity-categories", label: "Activity Categories" },
+  { href: "/academic-years", label: "Academic Years" },
+];
+
+const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const { darkMode, toggleDarkMode } = useContext(ThemeContext);
+
+  const ToggleOpenMenu = () => {
+    setOpen((prevState) => !prevState);
+  };
+
+  const menuStyles = open ? "menu open" : "menu";
+
+  return (
+    <div className="navbar">
+      <div className="brand">
+        <span>Social Work Management</span>
+      </div>
+      <div className={menuStyles}>
+        <ul>
+          {links.map((item) => (
+            <li key={item.href} onClick={ToggleOpenMenu}>
+              <Link to={item.href}>{item.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="hamburger">
+        <Menu onClick={ToggleOpenMenu} />
+      </div>
+      <div className="toggle">
+        <ToggleButton
+          value={"check"}
+          selected={darkMode}
+          onChange={toggleDarkMode}
+        >
+          {darkMode ? <LightMode /> : <DarkMode />}
+        </ToggleButton>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
